f82a80e797e1f731271203e2c26b62770ba7bf65
Meher Virk <mehervirk@gmail.com>
2024-02-26 08:38:42 -0800

    Hid my creature
    Hid my creature behind the Pac man in the code

--- src/sketch.js
+++ src/sketch.js
@@ -21,6 +21,9 @@ function setup() {
 function draw() {
   background(b);
 
+   //Calling Whale
+   drawWhale(46,241);
+
   // drawghosts
   if (ghost == false)
   drawGhosts (250,210,0.25);
@@ -119,4 +122,60 @@ function drawCherry(x,y,s){
 function mouseClicked() {
   pacSpeed = 1;
 }
+
+function drawWhale(Wx,Wy){
+  push();
+  translate(Wx,Wy);
+  scale(0.15);
+  
+  
+     noStroke();
+    fill(176, 199, 240);
+    rect(-40, -210, 100, 65);
+  
+    //Front of body
+    fill(176, 199, 240);
+    quad(-170, -240, -30, -200-10, -30, -145, -110, -145);
+  
+    //End of body
+    // (3,1,2)
+    fill(176, 199, 240);
+    triangle(160, -115, 60, -210, 60, -145);
+  
+    //Tail 
+    scale(0.5);
+    fill(176, 199, 240);
+    triangle(300, -125, 320, -230, 350, -165);
+    fill(109, 132, 168);
+    triangle(250, -200, 320, -230, 250, -145);
+   
+    //Foreground Arm 
+    //(3,1,2)
+    fill(176, 199, 240);
+    triangle(50, -200-10, -40, -290, 40, -290);
+    triangle(50, -200-10, 40, -270, 110, -195);
+  
+    //Background Arm
+    fill(109, 132, 168);
+    triangle(-50, -210, -140, -290, -60, -290);
+    triangle(-50, -210, -60, -270, -10, -195);
+  
+    //Back support 
+    //(3,1,2)
+    fill(176, 199, 240);
+    triangle(-30, -410, -343, -479, 118, -420);
+  
+    //eye 
+    fill(2, 42, 56);
+    ellipse(-160, -330, 20, 10);
+  
+    //face 
+    fill(2, 42, 56);
+    ellipse(-210, -310, 50, 2);
+    
+   pop(); 
+
+}
+
+
 
